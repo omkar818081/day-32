d6cac25d9d6096014483a77650d9b00daf38bfd6
omkar818081 <148517611+omkar818081@users.noreply.github.com>
2023-10-20 09:54:12 +0530

    Add files via upload

---
+++ ok.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+void main ()
+{
+    int n;
+    printf("enter value of n1");
+    scanf("%d",&n);
+     
+     while (n >= 1);
+     {
+        printf ("%d\n",n);
+        n--;
+        
+     }
+}
